8909c22b407fc64e45bcd755e9ad342d7b3dcd94
Elijah Wilson <elijahwilsonnn@gmail.com>
2024-12-15 22:40:49 -0800

    update setup module

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-# setup.py
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import numpy as np
+import numpy
 
 extensions = [
-    Extension("ising_helpers", ["ising_helpers.pyx"],
-              include_dirs=[np.get_include()],
-              extra_compile_args=["-O3"])
+    Extension(
+        name="ising_helpers",
+        sources=["ising_helpers.pyx"],
+        language="c",
+        extra_compile_args=["-O3", "-march=native"],  # Optimization flags
+        include_dirs=[numpy.get_include()],
+        # Uncomment the following lines to enable OpenMP (if desired)
+        # extra_compile_args=["-O3", "-fopenmp"],
+        # extra_link_args=["-fopenmp"],
+    )
 ]
 
 setup(
+    name="ising_helpers",
     ext_modules=cythonize(extensions, language_level=3),
-    include_dirs=[np.get_include()]
 )
